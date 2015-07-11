0e7b99c220be48ed7a08cb2f6731aab1b5889ef8
kiarabol <kiarabol@hotmail.com>
2015-07-12 03:52:27 +0900

    Wire in Myo

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ server.listen(port);
 
 console.log('http server listening on %d', port);
 
-
 // Send Myo activity to a web page
 myoEvents = ['fist', 'thumb_to_pinky', 'connected', 'double_tap'];
 
@@ -24,43 +23,23 @@ myoEvents.map(function(e, i, c) {
   });
 });
 
-
-// // Set up a new output for displaying sound
-// var output = new midi.output();
-// // This needs to be configured either for Bluetooth or network
-// var portsCount = output.getPortCount();
-
-// if (portsCount > 0) {
-//   output.getPortName(0);
-//   output.openPort(0);
-//   myMyo.on('gyroscope', function(data) {
-//     output.sendMessage([(data.x % 127), (data.y % 127), 1]);
-//   });
-// }
-// // output.closePort();
-
 var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
 
 wss.on('connection', function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(
-          {
-              'date': new Date(),
-              'point': {
-                  'x': Math.random() * 200 - 100,
-                  'y': Math.random() * 200 - 100
-              }
-          }));
-  }, 100);
+
+  console.log('websocket connection open');
 
   // Pass Myo gesture activity over to the web UI
   myMyo.on('gyroscope', function(data) {
-        console.log(new Date(), 'gyroscope', data);
-        ws.send(JSON.stringify(new Date()), data);
+        // console.log(new Date(), 'gyroscope', data);
+		
+		ws.send(JSON.stringify(
+          {
+              'date': new Date(),
+              'point': data
+          }));
   });
-
-  console.log('websocket connection open');
 
   ws.on('close', function() {
     console.log('websocket connection close');
