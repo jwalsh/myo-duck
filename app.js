0c3e48d25ce5ceb1d87ea6efe4fa417e82a49bf7
Jason Walsh <j@wal.sh>
2015-07-10 22:12:02 -0400

    Logging

--- app.js
+++ app.js
@@ -7,21 +7,22 @@ console.log('app.js');
 // var events = ['fist', 'gyroscope'];
 
 myMyo.on('fist', function(edge){
-    console.log('fist');
+    console.log(new Date(), 'fist', edge);
 });
 
 myMyo.on('gyroscope', function(data){
-    console.log('gyroscope');
+    console.log(new Date(), 'gyroscope', Math.ceil(data.x),
+                Math.ceil(data.y));
 });
 
 myMyo.on('thumb_to_pinky', function(edge){
-    console.log('thumb_to_pinky');
+    console.log(new Date(), 'thumb_to_pinky', edge);
 });
 
-myMyo.on('connected', function () {
-    console.log('connected');
+myMyo.on('connected', function (evt) {
+    console.log(new Date(), 'connected', evt);
 });
 
 myMyo.on('double_tap', function (edge) {
-    console.log('double_tap');
+    console.log(new Date(), 'double_tap', edge);
 });
