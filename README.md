6fbbcf776ac575821aaf943f57e6e57650d9f044
Jason Walsh <j@wal.sh>
2015-07-10 21:24:01 -0400

    Debugging for Myo events

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # myo-duck
+
+## Setup 
+
+1. Download Myo Connect 
+  https://developer.thalmic.com/downloads
+2. 
---
+++ app.js
@@ -0,0 +1,27 @@
+var Myo = require('myo');
+
+var myMyo = Myo.create();
+
+console.log('app.js');
+
+// var events = ['fist', 'gyroscope'];
+
+myMyo.on('fist', function(edge){
+    console.log('fist');
+});
+
+myMyo.on('gyroscope', function(data){
+    console.log('gyroscope');
+});
+
+myMyo.on('thumb_to_pinky', function(edge){
+    console.log('thumb_to_pinky');
+});
+
+myMyo.on('connected', function () {
+    console.log('connected');
+});
+
+myMyo.on('double_tap', function (edge) {
+    console.log('double_tap');
+});
