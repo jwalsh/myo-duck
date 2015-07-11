cf5e391988a8b143e39c803d92b0a264d9d7e1b0
Jason Walsh <j@wal.sh>
2015-07-11 02:03:18 -0400

    Mock out the events that would fire from Myo

--- README.md
+++ README.md
@@ -4,4 +4,11 @@
 
 1. Download Myo Connect 
   https://developer.thalmic.com/downloads
-2. 
+2. Configure local Midi settings 
+3. Update and run the application 
+
+    npm install
+    npm start 
+    open http://localhost:5000/
+    
+4. Validate that pings are seen and that gestures are painted
--- index.js
+++ index.js
@@ -1,82 +1,69 @@
 var Myo = require('myo');
 var midi = require('midi');
 var myMyo = Myo.create();
-var WebSocketServer = require("ws").Server;
-var http = require("http");
-var express = require("express");
+var WebSocketServer = require('ws').Server;
+var http = require('http');
+var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
-console.log('app.js starting');
-
-
-app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + '/'));
 
 var server = http.createServer(app);
 server.listen(port);
 
-console.log("http server listening on %d", port);
+console.log('http server listening on %d', port);
 
-var wss = new WebSocketServer({server: server});
-console.log("websocket server created");
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-      ws.send(JSON.stringify(new Date()), function() {  });
-  }, 1000);
+// Send Myo activity to a web page
+myoEvents = ['fist', 'thumb_to_pinky', 'connected', 'double_tap'];
 
-    console.log("websocket connection open");
-
-  ws.on("close", function() {
-      console.log("websocket connection close");
-      clearInterval(id);
+myoEvents.map(function(e, i, c) {
+  myMyo.on(e, function(evt) {
+        console.log(new Date(), e, evt);
   });
 });
 
 
-myMyo.on('fist', function(edge){
-    console.log(new Date(), 'fist', edge);
-});
-
-myMyo.on('gyroscope', function(data){
-    console.log(new Date(), 'gyroscope',
-                Math.ceil(data.x),
-                Math.ceil(data.y));
-});
-
-myMyo.on('thumb_to_pinky', function(edge){
-    console.log(new Date(), 'thumb_to_pinky', edge);
-});
-
-myMyo.on('connected', function (evt) {
-    console.log(new Date(), 'connected', evt);
-});
-
-myMyo.on('double_tap', function (edge) {
-    console.log(new Date(), 'double_tap', edge);
-});
-
-
-
-// Set up a new output.
+// Set up a new output for displaying sound
 var output = new midi.output();
-
-// Count the available output ports.
+// This needs to be configured either for Bluetooth or network
 var portsCount = output.getPortCount();
 
-// Get the name of a specified output port.
-
-
-// Open the first available output port.
 if (portsCount > 0) {
-    output.getPortName(0);
-    output.openPort(0);
-    myMyo.on('gyroscope', function(data){
-        output.sendMessage([(data.x % 127),(data.y % 127),1]);
-    });
+  output.getPortName(0);
+  output.openPort(0);
+  myMyo.on('gyroscope', function(data) {
+    output.sendMessage([(data.x % 127), (data.y % 127), 1]);
+  });
 }
-
-
-
-// Close the port when done.
 // output.closePort();
+
+var wss = new WebSocketServer({server: server});
+console.log('websocket server created');
+
+wss.on('connection', function(ws) {
+  var id = setInterval(function() {
+      ws.send(JSON.stringify(
+          {
+              'date': new Date(),
+              'point': {
+                  'x': Math.random() * 200 - 100,
+                  'y': Math.random() * 200 - 100
+              }
+          }));
+  }, 1000);
+
+    // Pass Myo gesture activity over to the web UI
+    myMyo.on('gyroscope', function(data) {
+        console.log(new Date(), 'gyroscope', data);
+        ws.send(JSON.stringify(new Date()), data);
+    });
+
+  console.log('websocket connection open');
+
+  ws.on('close', function() {
+    console.log('websocket connection close');
+    clearInterval(id);
+  });
+});
