0a55a4b676b05fd76ee49f4cd4f0596c03b9aa5c
Jason Walsh <j@wal.sh>
2015-07-12 02:23:33 -0400

    Cases for partial times for the interval and 5s

--- myo-gyroscope-mock.js
+++ myo-gyroscope-mock.js
@@ -19,8 +19,8 @@ var position = function(t, r, origin, rotationTime) {
     // console.log('position()', t, r, origin, rotationTime);
     var angle =  2 * Math.PI * (t / rotationTime);
     var result = {
-        x: origin.x + r * Math.cos(angle),
-        y: origin.y + r * Math.sin(angle)
+        x: parseFloat((origin.x + r * Math.cos(angle)).toPrecision(6)),
+        y: parseFloat((origin.y + r * Math.sin(angle)).toPrecision(6))
     };
     // console.log('position() result: ', result);
     return result;
@@ -41,8 +41,14 @@ expect('int', 1, 1);
 expect('object', {x: 1}, {x: 1});
 
 // Test harness
+// We go around the circle in one second so each of the returned
+// positions should be 90 degress off
 expect('0', mockPosition(0), {x: 3, y: 1.5});
 expect('1000', mockPosition(1000), {x: 3, y: 1.5});
+expect('500', mockPosition(500), {x: 0, y: 1.5});
+expect('250', mockPosition(250), {x: 1.5, y: 3});
+expect('750', mockPosition(750), {x: 1.5, y: 0});
+expect('5750', mockPosition(5750), {x: 1.5, y: 0});
 
 // Test run for two seconds
 // for (var i = 0; i < 2000; i+= 30) {
